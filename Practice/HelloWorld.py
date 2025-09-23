3655ed82e228a7ae720632bb248871736c2758aa
Fry Cole <825809@stma.k12.mn.us>
2025-09-23 13:14:33 -0500

    Initial commit got me feeling freaky

--- Practice/HelloWorld.py
+++ Practice/HelloWorld.py
@@ -1,7 +1,11 @@
 # Data Types
 # Boolean - true/false
-# Character 
-
+# Character - a sigle keyboard symbol
+# String 
+# Syntax - Grammar
+# Statement - Instruction 
+# Green inside function is called a paramater
+# 
 print("Hello World!")
 print("Hello Fry!")
 
