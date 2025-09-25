8e8f53d3ac3dd124c0ac97bf5145f6edf2170e47
Fry Cole <825809@stma.k12.mn.us>
2025-09-25 14:12:09 -0500

    2.4 notes

---
+++ Practice/2.4-ArthmeticOperators.py
@@ -0,0 +1,5 @@
+# Modulus (%) Returns the remainder of the division of two operands
+# e.g. 5 % 3
+# Exponentation symbol (**)
+# Floor Division (//) Divides and rounds down to the nearest integer 
+# 10//3 gives 3
