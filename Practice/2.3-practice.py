605edfd22747a74f1a1152b5d6ba31f6cb254bca
Fry Cole <825809@stma.k12.mn.us>
2025-09-25 13:14:58 -0500

    2.3 Practice

---
+++ Practice/2.3-practice.py
@@ -0,0 +1,45 @@
+# String    - "in quatations"
+# Integer   - a whole number
+# Float     - a decimal number 
+# Character - a single glyph 
+# Boolean   - true or false
+# Parameter - information needed for a function to work ex. I say jump, you say how high.
+# Parameter - Information stating how high (reference prior line)
+
+# How to define a variable
+# <name> = <value> 
+# Example: x = 10
+# No special characters in variables
+# Example below
+
+pascbvaddmsavsacadchmmvg=3
+
+
+# Math symbols are +,-,*,/ (addition, subtraction, multiplication, division)
+x=10
+y=50
+potato=7
+
+print(x+y+potato)
+
+# Naming conventions listed below       (for naming variables)
+lowercase = False
+UPPERCASE= False
+camelCase = False   #All lowercase, no spaces, capital for new words
+UppercamelCase = False
+lowerCamelCase = False
+kebab-case = False
+SCREAMING_SNAKE_CASE = False
+
+# Python's naming convention is snake_case
+# You are expected to use snake_case
+snake_case = True       #All lowercase, underscores for spaces
+
+# Other general rules to name things
+# 1. Concise        (short and descriptive)
+#   -
+
+the_font_size_for_paragraphs = 10       # too descriptive
+font_size_para = 10
+para_size = 10
+para = 10                               # too short
