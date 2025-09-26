f9039ae5944ded1dce78e0c39176e5344dd351b6
Fry Cole <825809@stma.k12.mn.us>
2025-09-26 14:15:28 -0500

    Animal Survey

--- Practice/2.4-ArthmeticOperators.py
+++ Practice/2.4-ArthmeticOperators.py
@@ -3,3 +3,7 @@
 # Exponentation symbol (**)
 # Floor Division (//) Divides and rounds down to the nearest integer 
 # 10//3 gives 3
+
+#Pyhton Concatenation 
+# The process of joining strings together. In python the + operator combines multiple strings
+# When concatenating all items must be strings, to use anothe data type use str()
