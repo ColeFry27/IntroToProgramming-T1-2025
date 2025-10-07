034ab0058a806c4f4b0cf6b4a5d85bf03fb9bc6a
Fry Cole <825809@stma.k12.mn.us>
2025-10-07 14:12:08 -0500

    Boolean

---
+++ Assignments/BooleanQuiz.py
@@ -0,0 +1,26 @@
+question_one = int(input("Give a number greater than 0.\n> "))
+print(int(question_one) > 0)
+
+question_two = int(input("Give a number less than 4?\n> "))
+print(int(question_two) < 4)
+
+question_three = int(input("Give a number equal to 10\n> "))
+print(int(question_three) == 10)
+
+question_four = int(input("Give a number less than or equal to 50\n> "))
+print(int(question_four) <= 50)
+
+question_five = int(input("Give a number greater than or equal to 123\n> "))
+print(int(question_five) >= 123)
+
+question_six = int(input("Give a number less than 2 or greater than to 5\n> "))
+print(int(question_six) > 5 or int(question_six) < 2)
+
+question_seven = int(input("Give a number greater than or equal to 90 or equal to 135\n> "))
+print(question_seven >= 90 or question_seven == 135)
+
+question_eight = int(input("Give anumber greater than 30 but less than 35\n> "))
+print(question_eight > 30 and question_eight < 35)
+
+question_nine = int(input("Give a number unequal to 55\n> "))
+print(question_nine != 55)
---
+++ Practice/3.1 Boolean Expressions.py
@@ -0,0 +1,39 @@
+#   Boolean expression is like a question that your computer can answer with a "yes" or "no"
+#   Used to represent logical statements that can be either true or false
+#   A comparision operator is a symbol used to compare two values,returninfg a Boolean Expression(Condition)
+'''
+Comparision Operators
+    Equal to (==)
+    Checks if two operands are equal. e.g. 5 == 3 results in false
+
+    Greater than (>)
+
+    
+    Less than  (<)
+
+    
+    Greater than  or equal to (>=)
+
+    
+    less than  or equal to (<=)
+
+
+    Not equal to (!=)
+
+    conversiting to integer is not necessary with == or !=
+'''
+
+'''
+Logical Operators
+A keywords used to COMBINE or INVERT a Boolean Expression returninga Boolean result based on the logical
+relationship between the expressions
+    
+    AND (and):
+    Returns true if both operands are true.     e.g. True and False results in False
+    
+    Or or):
+    Returns true if at least one of the operands is true.   e.g. True or False results in True
+    
+    NOT (not):
+    Reverses the logicial state of its operand.     e.g. not True results in False
+'''
