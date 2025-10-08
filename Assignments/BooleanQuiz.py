5d984851c5cdb8cda94e5c28c9e96aee6748ee06
Fry Cole <825809@stma.k12.mn.us>
2025-10-08 13:01:08 -0500

    Boolean Configuration

--- Assignments/BooleanQuiz.py
+++ Assignments/BooleanQuiz.py
@@ -5,7 +5,7 @@ question_two = int(input("Give a number less than 4?\n> "))
 print(int(question_two) < 4)
 
 question_three = int(input("Give a number equal to 10\n> "))
-print(int(question_three) == 10)
+print(question_three == 10)
 
 question_four = int(input("Give a number less than or equal to 50\n> "))
 print(int(question_four) <= 50)
@@ -17,10 +17,10 @@ question_six = int(input("Give a number less than 2 or greater than to 5\n> "))
 print(int(question_six) > 5 or int(question_six) < 2)
 
 question_seven = int(input("Give a number greater than or equal to 90 or equal to 135\n> "))
-print(question_seven >= 90 or question_seven == 135)
+print(int(question_seven) >= 90 or question_seven == 135)
 
 question_eight = int(input("Give anumber greater than 30 but less than 35\n> "))
-print(question_eight > 30 and question_eight < 35)
+print(int(question_eight) > 30 and int(question_eight) < 35)
 
 question_nine = int(input("Give a number unequal to 55\n> "))
 print(question_nine != 55)
