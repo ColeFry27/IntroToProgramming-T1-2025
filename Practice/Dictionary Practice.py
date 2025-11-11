b6f63a19a55859bce80b515735eee51770265527
Fry Cole <825809@stma.k12.mn.us>
2025-11-11 13:21:23 -0600

    Dictionaries Practice

--- Practice/Dictionary Practice.py
+++ Practice/Dictionary Practice.py
@@ -18,6 +18,32 @@ student["grade"] = "A+"
 print(student)
 
 movies = {
-    "Silence of the Lambs": "1991"
-    
+    "Silence of the Lambs": "1991",
+    "The Batman": "2022",
+    "The Lego Movie": "2014"
 }
+
+user_movie = input("Add a movie to the list!\n> ")
+release_year = input("What year did it release?\n> ")
+movies[user_movie] = release_year
+print(movies)
+
+fruits = {
+    "Apple": 0.79,
+    "Kiwi": 1.79,
+    "Tangerine": 2.09,
+    "Grapes": 4.19,
+    "Strawberry": 6.89
+}
+remove_fruit = input("Remove a fruit from the list below!\n Apple,\n Kiwi,\n Tangerine,\n Grapes,\n Strawberry\n\n> ")
+if remove_fruit.lower == "apple":
+    fruits.pop("Apple")
+elif remove_fruit.lower == "kiwi":
+    fruits.pop("Kiwi")
+elif remove_fruit.lower == "tangerine":
+    fruits.pop("Tangerine")
+elif remove_fruit.lower == "grapes":
+    fruits.pop("Grapes")
+elif remove_fruit.lower == "strawberry":
+    fruits.pop("Strawberry")
+print(fruits)
